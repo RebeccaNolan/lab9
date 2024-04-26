29eeb7ab5867e68ab9db64e0951f38badaa7098f
unknown <g00423921@atu.ie>
2024-04-26 11:45:48 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, FormsModule, IonButton],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  coordinates: any = "";
+  lat: string = "";
+  long: String = "";
+  time: any = 0;
+
   constructor() {}
+
+  async getGPS() {
+    this.coordinates = await Geolocation.getCurrentPosition();
+    this.lat = this.coordinates.coords.latitude;
+    this.long = this.coordinates.coords.longitude;
+    this.time = new Date(this.coordinates.timestamp);
+  }
+
+  async openBrowser(){
+    await Browser.open({ url:  'http://capacitorjs.com/' })
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
